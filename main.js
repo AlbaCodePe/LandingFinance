48611b9a3029229b0d11097581c16d933205b210
AlbaCodePe <u202015623@upc.edu.pe>
2024-04-21 15:32:12 -0500

    version basic without forms

---
+++ main.js
@@ -0,0 +1,49 @@
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+let menuToggler=selectElement('.menu-toggle');
+let body=selectElement('body');
+
+menuToggler.addEventListener('click', function() {
+    body.classList.toggle('open');
+});
+
+menuToggler.addEventListener('blur',function(){
+    body.classList.toggle('close');
+});
+
+window.sr=ScrollReveal();
+
+sr.reveal('.animate-left',{
+    origin:'left',
+    duration:1000,
+    distance:'25rem',
+    delay:300
+});
+
+sr.reveal('.animate-right',{
+    origin:'right',
+    duration:1000,
+    distance:'25rem',
+    delay:600
+});
+
+sr.reveal('.animate-top',{
+    origin:'top',
+    duration:1000,
+    distance:'25rem',
+    delay:600
+});
+
+sr.reveal('.animate-bottom',{
+    origin:'bottom',
+    duration:1000,
+    distance:'25rem',
+    delay:600
+});
+function changeClass()
+{
+
+    document.getElementById('body').className="";
+}
